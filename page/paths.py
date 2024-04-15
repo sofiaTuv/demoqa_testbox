297c39ef3a548fa39f1a0ca908ecd38f4b9157f2
Sofia Tuvykina <hdfaa.56@gmail.com>
2024-04-15 18:38:48 +0300

    links

---
+++ page/paths.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+class Paths:
+    FULL_NAME = (By.XPATH, '//input[@id="userName"]')
+    EMAIL = (By.XPATH, '//input[@id="userEmail"]')
+    CURRENT_ADDRESS = (By.XPATH, '//textarea[@id="currentAddress"]')
+    PERMANENT_ADDRESS = (By.XPATH, '//textarea[@id="permanentAddress"]')
+    SUBMIT = (By.XPATH, '//button[@id="submit"]')
+    CHECK_NAME = (By.CSS_SELECTOR, '#name')
+    CHECK_EMAIL = (By.CSS_SELECTOR, '#email')
+    CHECK_CUR_ADDRESS = (By.CSS_SELECTOR, '#currentAddress')
+    CHECK_PER_ADDRESS = (By.CSS_SELECTOR, '#permanentAddress')
