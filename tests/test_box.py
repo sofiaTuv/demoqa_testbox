83f65b43f2d4c315be73a900f4992c1bda130b9b
Sofia Tuvykina <hdfaa.56@gmail.com>
2024-04-15 18:39:37 +0300

    test demoqa tz

---
+++ tests/test_box.py
@@ -0,0 +1,42 @@
+from helper import Helper
+from page.methods_page import MethodsPage
+from page.paths import Paths
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+class TestDemoQA:
+    def test_box(self, browser):
+        # ARRANGE
+        page = MethodsPage(browser)
+        page.open()
+
+        # ACT
+        f_name = Helper.generate_name()
+        page.enter_full_name(f_name)
+
+        email = Helper.generate_email()
+        page.enter_email(email)
+
+        c_address = Helper.generate_address()
+        page.enter_current_address(c_address)
+
+        p_address = Helper.generate_address()
+        page.enter_permanent_address(p_address)
+
+        page.click_submit()
+
+        # ASSERT
+        name_text = page.get_name_text()
+        email_text = page.get_email_text()
+        c_address_text = page.get_current_address()
+        p_address_text = page.get_permanent_address()
+
+        assert f_name in name_text
+        assert email in email_text
+        assert c_address in c_address_text
+        assert p_address in p_address_text
+
+
+
+
